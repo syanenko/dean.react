7d03f7cbafdd407233abfbf7b8e28006b428376b
syanenko65 <syanenko65@gmail.com>
2021-02-08 03:47:15 +0300

    Move to unity folder

---
+++ unity/Assets/Scenes/islands/Scripts/Clouds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clouds : MonoBehaviour {
+
+    /* Use defult settings
+    void Start ()
+	{
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "Clouds";
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = -1;
+	}*/
+}
---
+++ unity/Assets/Scriprs/Exit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+            Debug.Log("Quit !");
+            
+            // Ask via dialog here
+			Application.Quit();
+		}
+	}
+}
