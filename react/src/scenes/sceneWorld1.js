52d5450ae35270ae31496a1b398ed9c5ae8fd370
Sergey Yanenko <syanenko65@gmail.com>
2024-01-05 21:38:36 +0300

    VR render at full window, common, build.bat

--- react/src/scenes/sceneWorld1.js
+++ react/src/scenes/sceneWorld1.js
@@ -1,13 +1,13 @@
 //
-// Worlds
+// Worlds test scene
 //
 import React from 'react';
+
 import * as THREE from 'three';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
+// var resolution = new THREE.Vector2(1200, 750);
+var resolution = new THREE.Vector2(window.innerWidth, window.innerHeight);
 // import { resolution, materials } from './materials';
-
-var resolution = new THREE.Vector2(1200, 750);
-
 //
 // Materials
 //
@@ -24,17 +24,18 @@ class sceneWorld1 extends React.Component
     const scene = new THREE.Scene();
     
     // Camera
-    const camera = new THREE.PerspectiveCamera( 70, resolution.x / resolution.y, 0.01, 1000 );
+    const camera = new THREE.PerspectiveCamera( 70, resolution.x / resolution.y, 1, 1000 );
     camera.layers.enable( 1 );
-    
+        
     // Renderer
     const renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(resolution.x, resolution.y);
-    // renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    
+    // DEBUG !
+    // this.mount.appendChild(renderer.domElement);
+    document.getElementById('root').appendChild(renderer.domElement);
 
-    this.mount.appendChild(renderer.domElement);
-       
     // XR
     renderer.xr.enabled = true;
     renderer.xr.setReferenceSpaceType( 'local' );
@@ -44,9 +45,9 @@ class sceneWorld1 extends React.Component
     } );
     
     renderer.xr.addEventListener( 'sessionend', function ( event ) {
-      // TODO: Set proper view
-      renderer.setSize(resolution.x, resolution.y);
-      // unmountComponent (this);
+      // TODO: Delete 'this' or rendered  component here
+      // document.getElementById('root').remove(renderer.domElement);
+      window.location.reload();
     } );
 
     // VR Button
@@ -58,13 +59,16 @@ class sceneWorld1 extends React.Component
     this.mount.appendChild( vrButton );
 
     // Geometry
+    // DEBUG !
+    // const geometry = new THREE.SphereGeometry(50);
+    
     const geometry = new THREE.BoxGeometry( 100, 100, 100 );
     geometry.scale( 1, 1, - 1 );
 
     const textures = getTexturesFromAtlasFile( 'data/textures/sun_temple_stripe.jpg', 12 );
+    // const textures = getTexturesFromAtlasFile( 'data/textures/Sea_star_2560x1440_VR180.jpg', 12 );
     const materials = [];
     for ( let i = 0; i < 6; i ++ ) {
-
       materials.push( new THREE.MeshBasicMaterial( { map: textures[ i ] } ) );
     }
 
@@ -74,7 +78,6 @@ class sceneWorld1 extends React.Component
     
     const materialsR = [];
     for ( let i = 6; i < 12; i ++ ) {
-
       materialsR.push( new THREE.MeshBasicMaterial( { map: textures[ i ] } ) );
     }
 
@@ -130,7 +133,7 @@ class sceneWorld1 extends React.Component
       camera.updateProjectionMatrix();
 
       // renderer.setSize( window.innerWidth, window.innerHeight );
-      // renderer.setSize(resolution.x, resolution.y);
+      renderer.setSize(resolution.x, resolution.y);
     }
 
     //
